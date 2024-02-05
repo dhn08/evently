70f0560497ceae80e03b93c167700a5be893de9f
Dhananjay <singhdhananjay.2001@gmail.com>
2024-02-06 00:02:42 +0530

    Correcteda error in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/events:id",
+    "/events/:id",
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
